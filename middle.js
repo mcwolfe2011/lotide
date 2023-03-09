43469dbe47734f0571cf107d412ae6fbcb745e81
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-08 20:25:29 -0800

    Adding middle.js

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+const middle = function(array) {
+  let midItems = [];
+  if (array.length === 1 || array.length === 2) {
+    return [];
+  }
+  let middleIndex = Math.floor(array.length / 2);
+  if (array.length % 2 === 0) {
+    midItems.push(array[middleIndex - 1]);
+    midItems.push(array[middleIndex]);
+  } else {
+    midItems.push(array[middleIndex]);
+  }
+  return midItems;
+};
+
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2])); // => []
+let equality = assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+console.log(equality); // Assertion Passed
