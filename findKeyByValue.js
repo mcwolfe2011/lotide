7df82bd6e05ac7b972c817381eac53a5d467ccbb
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-10 20:45:05 -0800

    Adding findKey.js and modified findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -20,7 +20,8 @@ const bestTVShowsByGenre = {
 const findKeyByValue = function(object, value) {
   const keysArray = Object.keys(object);
   for (let element of keysArray) {
-    if (object[element] === value) return element;
+    if (object[element] === value)
+      return element;
   }
 };
 
