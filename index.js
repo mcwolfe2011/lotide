a95c43e03d82be86b228233adfacb389e020fb51
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-16 06:25:17 -0700

    did some changes in index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+const index = function() {
+  const head = require('./head');
+  const tail = require('./tail');
+  const middle = require('./middle');
 };
+
+module.exports = { head, tail, middle };
 
